c15c6487308969dd05c672d6dafc66ec442132c1
Delwer Hossain <delwerhossain006@gmail.com>
2023-05-02 22:16:34 +0600

    auth setupment done

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes";
+import AuthProvider, { AuthContext } from "./provider/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <AuthProvider>
     <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import {
   getAuth,
   createUserWithEmailAndPassword,
