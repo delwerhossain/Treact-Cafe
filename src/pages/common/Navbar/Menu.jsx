1ba2d136be7b6cbe694d15cdea199cf6ee7c661d
delwerHossain <delwerhossain006@gmail.com>
2023-05-02 12:41:08 +0100

    fixed links

--- src/pages/common/Navbar/Menu.jsx
+++ src/pages/common/Navbar/Menu.jsx
@@ -62,7 +62,7 @@ const Navbar = () => {
               </ul>
             </div>
             <div className="navbar-end">
-              <Link className="btn">Login</Link>
+              <Link to={'login'} className="btn">Login</Link>
             </div>
           </div>
     );
