198be45785982fc65bab3b1d555ee602c6a58d5f
delwerHossain <delwerhossain006@gmail.com>
2023-05-02 14:12:48 +0100

    primary color added

--- src/pages/common/Navbar/Menu.jsx
+++ src/pages/common/Navbar/Menu.jsx
@@ -62,7 +62,7 @@ const Navbar = () => {
               </ul>
             </div>
             <div className="navbar-end">
-              <Link to={'login'} className="btn">Login</Link>
+              <Link to={'login'} className="btn btn-primary">Login</Link>
             </div>
           </div>
     );
