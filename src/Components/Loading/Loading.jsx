8ecc3130c08e3a202e1924589477679c0be4a907
Delwer Hossain <delwerhossain006@gmail.com>
2023-05-04 21:41:35 +0600

    size fix on spinner

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -4,7 +4,7 @@ import { DotLoader } from "react-spinners";
 const Loading = () => {
   return (
     <div id="spinner" className="sweet-loading h-screen flex justify-center items-center">
-      <DotLoader color="#600cfc" loading size={200} speedMultiplier={2} />
+      <DotLoader color="#600cfc" loading size={250} speedMultiplier={2} />
     </div>
   );
 };
